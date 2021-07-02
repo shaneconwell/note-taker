0acf7cea17fb59337c74e561dab6d87ded189d90
hotbagels <81516937+hotbagels@users.noreply.github.com>
2021-07-01 22:14:06 -0400

    ran into a big blocker that held me up big time. express.static to the rescue. App is now accepting input data from the notes page into the db.json

--- express.js
+++ express.js
@@ -1,37 +1,62 @@
 const express = require('express');
 const path = require('path');
-const notes = require('./db/db.json')
-
-
-
+const notes = require('./db.json')
 const fs = require('fs');
 
 // npm for generating a unique ID
 const { v4: uuidv4 } = require('uuid');
-
 const app = express();
 const id = uuidv4();
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
+const dbRead = fs.readFileSync('db.json')
+let dbParse = JSON.parse(dbRead)
+
+
+console.log(id);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
+app.use(express.static(__dirname + '/public'));
+// require('./public/assets/js/index')(fs);
 
 // GET routes
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 app.get('/api/notes', (req, res) => res.json(notes));
 
+// app.get('/api/notes/:noteID', (req, res) => {
+//     const chosen = req.params.character;
+  
+//     console.log(chosen);
+  
+//     for (let i = 0; i < notes.length; i++) {
+//       if (chosen === notes[i].noteID) {
+//         return res.json(notes[i]);
+//       }
+//     }
+  
+//     return res.json(false);
+//   });
+
 // FETCH routes
+// need to figure out the ID connection
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
-    newNote.routeName
-    notes.push(newNote);
-    res.json(newNote);
+    newNote.noteID = id;
+    dbParse.push(newNote);
+    // console.log(newNote);
+    let note = JSON.stringify(dbParse,null,2)
+    // console.log(note);
+    fs.writeFile('db.json', note, (err) =>
+    err ? console.error(err) : console.log('Commit logged!')
+    )
+    
+    res.send("OK")
 });
 
+
 app.listen(PORT, () => console.log(`App listening on PORT http://localhost:${PORT}`));
+
