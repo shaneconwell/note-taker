04db976dfc29dfa4594d47059007bf371f92ac34
hotbagels <81516937+hotbagels@users.noreply.github.com>
2021-07-01 23:59:38 -0400

    fixed a bug is the index.html,wrong path to index.js

--- express.js
+++ express.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
 // GET routes
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
