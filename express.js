cd428b8dd47e50a7f5796133da75e37164832a8a
hotbagels <81516937+hotbagels@users.noreply.github.com>
2021-07-01 22:17:26 -0400

    cleaned up some format and file structure

--- express.js
+++ express.js
@@ -5,20 +5,19 @@ const fs = require('fs');
 
 // npm for generating a unique ID
 const { v4: uuidv4 } = require('uuid');
+
 const app = express();
 const id = uuidv4();
 const PORT = process.env.PORT || 8080;
+
 
 const dbRead = fs.readFileSync('db.json')
 let dbParse = JSON.parse(dbRead)
 
 
-console.log(id);
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
-// require('./public/assets/js/index')(fs);
 
 // GET routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
@@ -27,22 +26,9 @@ app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.
 
 app.get('/api/notes', (req, res) => res.json(notes));
 
-// app.get('/api/notes/:noteID', (req, res) => {
-//     const chosen = req.params.character;
-  
-//     console.log(chosen);
-  
-//     for (let i = 0; i < notes.length; i++) {
-//       if (chosen === notes[i].noteID) {
-//         return res.json(notes[i]);
-//       }
-//     }
-  
-//     return res.json(false);
-//   });
 
 // FETCH routes
-// need to figure out the ID connection
+
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
     newNote.noteID = id;
